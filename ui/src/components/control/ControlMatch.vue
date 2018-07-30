72fb59d863fb2be0a4d6d7327def406f8f0c2e30
Nicolai Ommer <nicolai.ommer@qaware.de>
2018-07-30 09:26:50 +0200

    Add buttons for changing the stage

--- ui/src/components/control/ControlMatch.vue
+++ ui/src/components/control/ControlMatch.vue
@@ -2,6 +2,8 @@
     <div>
         <b-button v-on:click="resetMatch">Reset Match</b-button>
         <b-button v-on:click="switchColor">Switch colors</b-button>
+        <b-button v-on:click="previousStage">Previous Stage</b-button>
+        <b-button v-on:click="nextStage">Next Stage</b-button>
     </div>
 </template>
 
@@ -14,6 +16,12 @@
             },
             switchColor: function () {
                 this.$socket.sendObj({'trigger': {'triggerType': 'switchColor'}})
+            },
+            previousStage: function () {
+                this.$socket.sendObj({'stage': {'stageOperation': 'previous'}})
+            },
+            nextStage: function () {
+                this.$socket.sendObj({'stage': {'stageOperation': 'next'}})
             }
         }
     }
