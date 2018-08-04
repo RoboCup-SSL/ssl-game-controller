7d80f0e3b31962291df5997fc4fa5871247e06d4
Nicolai Ommer <nicolai.ommer@qaware.de>
2018-08-04 15:35:32 +0200

    [feature] Use packr to include UI files into binary

--- cmd/ssl-game-controller/main.go
+++ cmd/ssl-game-controller/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/g3force/ssl-game-controller/internal/app/controller"
+	"github.com/gobuffalo/packr"
 	"net/http"
 )
 
@@ -10,8 +11,9 @@ func main() {
 	g := controller.NewGameController()
 	g.Run()
 
-	// serve the static resource of UI (for production use only)
-	http.Handle("/", http.FileServer(http.Dir(".")))
+	box := packr.NewBox("../../ui/dist")
+	http.Handle("/", http.FileServer(box))
+
 	// serve the bidirectional web socket
 	http.HandleFunc("/ws", g.ApiServer.WsHandler)
 	http.ListenAndServe(":8081", nil)
