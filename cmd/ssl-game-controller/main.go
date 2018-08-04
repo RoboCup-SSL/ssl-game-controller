4305f8b32babc684f9539a91e7e29db3874a5031
Nicolai Ommer <nicolai.ommer@qaware.de>
2018-08-04 22:50:43 +0200

    [refactoring] Point to the UI URL, if compiled in

--- cmd/ssl-game-controller/main.go
+++ cmd/ssl-game-controller/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/RoboCup-SSL/ssl-game-controller/internal/app/controller"
 	"github.com/gobuffalo/packr"
+	"log"
 	"net/http"
 )
 
@@ -13,6 +14,12 @@ func main() {
 
 	box := packr.NewBox("../../ui/dist")
 	http.Handle("/", http.FileServer(box))
+
+	if box.Has("index.html") {
+		log.Print("UI is available at http://localhost:8081")
+	} else {
+		log.Print("Backend-only version started. Run the UI separately or get a binary that has the UI included")
+	}
 
 	// serve the bidirectional web socket
 	http.HandleFunc("/ws", g.ApiServer.WsHandler)
