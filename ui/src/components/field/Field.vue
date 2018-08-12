cc0ac1a4ce4d33adf935585039628963136b132a
Nicolai Ommer <nicolai.ommer@qaware.de>
2018-08-12 15:25:31 +0200

    [ui] Simplify field: Add lines, etc. as default values

--- ui/src/components/field/Field.vue
+++ ui/src/components/field/Field.vue
@@ -6,170 +6,45 @@
         <g :transform="getFieldTransformation">
 
             <!-- draw field background -->
-            <rect :x="-(fieldLength/2+boundaryWidth)"
-                  :y="-(fieldWidth/2+boundaryWidth)"
-                  :width="fieldLength+boundaryWidth*2"
-                  :height="fieldWidth+boundaryWidth*2"
-                  style="fill:green;fill-opacity:1;stroke:none"></rect>
+            <rect :x="-(field.fieldLength/2+field.boundaryWidth)"
+                  :y="-(field.fieldWidth/2+field.boundaryWidth)"
+                  :width="field.fieldLength+field.boundaryWidth*2"
+                  :height="field.fieldWidth+field.boundaryWidth*2"
+                  :style="{fill: 'green', fillOpacity: 1, stroke: 'none'}"></rect>
 
-            <!-- draw field borders -->
-            <line v-show="!useShapesFromGeometry"
-                  :x1="-fieldLength/2"
-                  :y1="-fieldWidth/2"
-                  :x2="-fieldLength/2"
-                  :y2="fieldWidth/2"
-                  style="stroke:white;stroke-width:10px"></line>
-            <line v-show="!useShapesFromGeometry"
-                  :x1="-fieldLength/2"
-                  :y1="fieldWidth/2"
-                  :x2="fieldLength/2"
-                  :y2="fieldWidth/2"
-                  style="stroke:white;stroke-width:10px"></line>
-            <line v-show="!useShapesFromGeometry"
-                  :x1="fieldLength/2"
-                  :y1="fieldWidth/2"
-                  :x2="fieldLength/2"
-                  :y2="-fieldWidth/2"
-                  style="stroke:white;stroke-width:10px"></line>
-            <line v-show="!useShapesFromGeometry"
-                  :x1="fieldLength/2"
-                  :y1="-fieldWidth/2"
-                  :x2="-fieldLength/2"
-                  :y2="-fieldWidth/2"
-                  style="stroke:white;stroke-width:10px"></line>
-            <line v-show="!useShapesFromGeometry"
-                  :x1="0"
-                  :y1="fieldWidth/2"
-                  :x2="0"
-                  :y2="-fieldWidth/2"
-                  style="stroke:white;stroke-width:10px"></line>
-            <!-- center circle -->
-            <circle v-show="!useShapesFromGeometry"
-                    :r="centerCircleRadius"
-                    style="stroke:white;stroke-width:10px;fill-opacity:0"></circle>
-            <!-- penalty areas -->
-            <rect v-show="!useShapesFromGeometry"
-                  :x="-(fieldLength)/2"
-                  :y="-(penAreaWidth)/2"
-                  :width="penAreaDepth"
-                  :height="penAreaWidth"
-                  style="stroke: white;stroke-width:10px; fill-opacity:0"></rect>
-            <rect v-show="!useShapesFromGeometry"
-                  :x="(fieldLength/2)-penAreaDepth"
-                  :y="-penAreaWidth/2"
-                  :width="penAreaDepth"
-                  :height="penAreaWidth"
-                  style="stroke: white;stroke-width:10px; fill-opacity:0"></rect>
-
-            <!-- goals -->
-            <line :x1="-fieldLength/2"
-                  :y1="-goalWidth/2"
-                  :x2="-fieldLength/2-goalDepth"
-                  :y2="-goalWidth/2"
-                  style="stroke:black;stroke-width:10px"></line>
-            <line :x1="-fieldLength/2"
-                  :y1="goalWidth/2"
-                  :x2="-fieldLength/2-goalDepth"
-                  :y2="goalWidth/2"
-                  style="stroke:black;stroke-width:10px"></line>
-            <line :x1="-fieldLength/2-goalDepth"
-                  :y1="-goalWidth/2"
-                  :x2="-fieldLength/2-goalDepth"
-                  :y2="goalWidth/2"
-                  style="stroke:black;stroke-width:10px"></line>
-
-            <line :x1="fieldLength/2"
-                  :y1="-goalWidth/2"
-                  :x2="fieldLength/2+goalDepth"
-                  :y2="-goalWidth/2"
-                  style="stroke:black;stroke-width:10px"></line>
-            <line :x1="fieldLength/2"
-                  :y1="goalWidth/2"
-                  :x2="fieldLength/2+goalDepth"
-                  :y2="goalWidth/2"
-                  style="stroke:black;stroke-width:10px"></line>
-            <line :x1="fieldLength/2+goalDepth"
-                  :y1="-goalWidth/2"
-                  :x2="fieldLength/2+goalDepth"
-                  :y2="goalWidth/2"
-                  style="stroke:black;stroke-width:10px"></line>
-
-            <!-- lines from geometry -->
-            <line v-for="l of lines"
-                  v-bind:key="l"
+            <line v-for="(l,i) of field.lines"
+                  :key="'line-' + i"
                   :x1="l.p1.x"
                   :y1="l.p1.y"
                   :x2="l.p2.x"
                   :y2="l.p2.y"
-                  style="stroke:white;stroke-width:10px"></line>
+                  :style="[defStyle, l]">
+            </line>
 
-            <!-- arcs from geometry (only circles supported for now -->
-            <circle v-for="a of arcs"
-                    v-bind:key="a"
+            <circle v-for="(a,i) of field.circles"
+                    :key="'circle-' + i"
                     :cx="a.center.x"
                     :cy="a.center.y"
                     :r="a.radius"
-                    style="stroke:white;stroke-width:10px;fill-opacity:0"></circle>
-
-            <!-- robots -->
-            <path v-for="r of getRobotsYellow"
-                  v-bind:key="r"
-                  :d="
-            'M ' + r.x + r.botRightX() + ',' + r.y + r.botRightY() +
-            'A ' + r.botRadius + ' ' + r.botRadius + ' 0 1 1 ' + r.x + r.botLeftX() + ',' + r.y + r.botLeftY() +
-            'L ' + r.x + r.botRightX() + ','+ r.y + r.botRightY()"
-                  style="stroke:black;stroke-width:0.3em;fill: yellow; fill-opacity:1"></path>
-
-            <path v-for="r of getRobotsBlue"
-                  v-bind:key="r"
-                  :d="
-            'M ' + r.x + r.botRightX() + ',' + r.y + r.botRightY() +
-            'A ' + r.botRadius + ' ' + r.botRadius + ' 0 1 1 ' + r.x + r.botLeftX() + ',' + r.y + r.botLeftY() +
-            'L ' + r.x + r.botRightX() + ','+ r.y + r.botRightY()"
-                  style="stroke:black;stroke-width:0.3em;fill: blue; fill-opacity:1"></path>
-
-            <!-- robot ids -->
-            <text v-for="r of getRobotsYellow"
-                  v-bind:key="r"
-                  :x="r.x"
-                  :y="r.y"
-                  :dx="'-40'"
-                  :dy="'45'"
-                  :textLength="r.botRadius+20"
-                  :lengthAdjust="'spacingAndGlyphs'"
-                  class="bot-id"
-                  style="stroke-width:0;fill: black">
-                {{r.id}}
-            </text>
-
-            <text v-for="r of getRobotsBlue"
-                  v-bind:key="r"
-                  :x="r.x"
-                  :y="r.y"
-                  :dx="'-40'"
-                  :dy="'45'"
-                  :textLength="r.botRadius+20"
-                  :lengthAdjust="'spacingAndGlyphs'"
-                  class="bot-id"
-                  style="stroke-width:0;fill: white">
-                {{r.id}}
-            </text>
-
-            <!-- balls -->
-            <circle v-for="r of balls"
-                    v-bind:key="r"
-                    :r="ballRadius"
-                    :cx="r.x"
-                    :cy="r.y"
-                    style="stroke:black;stroke-width:0.3em;fill: orangered; fill-opacity:1">
+                    :style="[defStyle, a]">
             </circle>
-            <circle v-for="r of balls"
-                    v-bind:key="r"
-                    :r="300"
-                    :cx="r.x"
-                    :cy="r.y"
-                    style="stroke:red;stroke-width:0.5em; fill-opacity:0">
-            </circle>
+
+            <path v-for="(p,i) of field.paths"
+                  :key="'path-' + i"
+                  :d="pathFromD(p.d)"
+                  :style="[defStyle, p]"></path>
+
+            <text v-for="(t,i) of field.texts"
+                  :key="'text-' + i"
+                  :x="t.p.x"
+                  :y="t.p.y"
+                  :dx="t.d.x"
+                  :dy="t.d.y"
+                  :textLength="t.textLength"
+                  :lengthAdjust="'spacingAndGlyphs'"
+                  :style="[defStyle, t]">
+                {{t.text}}
+            </text>
         </g>
     </svg>
 </template>
@@ -179,44 +54,131 @@
 
     export default {
         name: "Field",
-        data: function () {
-            return {
-                rotateField: true,
-                useShapesFromGeometry: false,
-                fieldWidth: 2000,
-                fieldLength: 3000,
-                boundaryWidth: 300,
-                penAreaWidth: 1000,
-                penAreaDepth: 500,
-                goalWidth: 600,
-                goalDepth: 180,
-                centerCircleRadius: 500,
-                ballRadius: 21.5,
-                lines: [{p1: {x: 100, y: 100}, p2: {x: 500, y: 1000}}],
-                arcs: [],
-                robotsYellow: {},
-                robotsBlue: {},
-                balls: []
+        props: {
+            rotateField: {
+                type: Boolean,
+                default: false
+            },
+            defStyle: {
+                type: Object,
+                default: function () {
+                    return {
+                        strokeWidth: 10,
+                        stroke: 'white',
+                        fillOpacity: 0
+                    }
+                }
+            },
+            field: {
+                type: Object,
+                default: function () {
+                    let fieldWidth = 2000;
+                    let fieldLength = 3000;
+                    let centerCircleRadius = 500;
+                    return {
+                        fieldWidth: fieldWidth,
+                        fieldLength: fieldLength,
+                        boundaryWidth: 300,
+                        penAreaWidth: 1000,
+                        penAreaDepth: 500,
+                        goalWidth: 600,
+                        goalDepth: 180,
+                        centerCircleRadius: centerCircleRadius,
+                        ballRadius: 21.5,
+                        lines: [
+                            {
+                                p1: {x: -fieldLength / 2, y: -fieldWidth / 2},
+                                p2: {x: -fieldLength / 2, y: fieldWidth / 2}
+                            },
+                            {
+                                p1: {x: -fieldLength / 2, y: fieldWidth / 2},
+                                p2: {x: fieldLength / 2, y: fieldWidth / 2}
+                            },
+                            {
+                                p1: {x: fieldLength / 2, y: fieldWidth / 2},
+                                p2: {x: fieldLength / 2, y: -fieldWidth / 2}
+                            },
+                            {
+                                p1: {x: fieldLength / 2, y: -fieldWidth / 2},
+                                p2: {x: -fieldLength / 2, y: -fieldWidth / 2}
+                            },
+                            {
+                                p1: {x: 0, y: fieldWidth / 2},
+                                p2: {x: 0, y: -fieldWidth / 2}
+                            }
+                        ],
+                        circles: [
+                            {
+                                center: {x: 0, y: 0},
+                                radius: centerCircleRadius,
+                                stroke: 'white',
+                                strokeWidth: 10,
+                                fill: '',
+                                fillOpacity: 0
+                            }
+                        ],
+                        paths: [
+                            {
+                                d: [
+                                    {
+                                        type: 'M',
+                                        args: [925, -550]
+                                    },
+                                    {
+                                        type: 'A',
+                                        args: [90, 90, 0, 1, 1, 925, -450]
+                                    },
+                                    {
+                                        type: 'L',
+                                        args: [925, -550]
+                                    }
+                                ],
+                                stroke: 'white',
+                                strokeWidth: '10',
+                                fill: 'yellow',
+                                fillOpacity: 1
+                            }
+                        ],
+                        texts: [
+                            {
+                                text: '1',
+                                p: {x: 990, y: -510},
+                                d: {x: -40, y: 45},
+                                textLength: 110,
+                                strokeWidth: 0,
+                                fill: 'black',
+                                font: 'bold 7em sans-serif',
+                                fillOpacity: 1
+                            }
+                        ]
+                    }
+                }
             }
         },
         computed: {
-            getRobotsYellow() {
-                return this.robotsYellow.values;
-            },
-            getRobotsBlue() {
-                return this.robotsBlue.values;
-            },
             getFieldTransformation() {
                 if (this.rotateField) {
-                    return 'rotate(90) scale(' + (this.fieldWidth / this.fieldLength) + ')';
+                    return 'rotate(90) scale(' + (this.field.fieldWidth / this.field.fieldLength) + ')';
                 }
                 return '';
             },
             viewBox() {
-                return (-(this.fieldLength / 2 + this.boundaryWidth))
-                    + ' ' + (-(this.fieldWidth / 2 + this.boundaryWidth))
-                    + ' ' + (this.fieldLength + this.boundaryWidth * 2)
-                    + ' ' + (this.fieldWidth + this.boundaryWidth * 2);
+                return (-(this.field.fieldLength / 2 + this.field.boundaryWidth))
+                    + ' ' + (-(this.field.fieldWidth / 2 + this.field.boundaryWidth))
+                    + ' ' + (this.field.fieldLength + this.field.boundaryWidth * 2)
+                    + ' ' + (this.field.fieldWidth + this.field.boundaryWidth * 2);
+            },
+        },
+        methods: {
+            pathFromD: function (pd) {
+                let d = '';
+                for (let s of pd) {
+                    d += s.type;
+                    for (let a of s.args) {
+                        d += ' ' + a
+                    }
+                }
+                return d;
             }
         }
     }
