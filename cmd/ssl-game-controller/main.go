69b3a3f3e99671f7b8676c9e971f2202629f6b5c
Nicolai Ommer <nicolai.ommer@qaware.de>
2018-08-17 22:32:21 +0200

    [cleanup] Remove vision-client backend integration

--- cmd/ssl-game-controller/main.go
+++ cmd/ssl-game-controller/main.go
@@ -3,20 +3,17 @@ package main
 import (
 	"flag"
 	"github.com/RoboCup-SSL/ssl-game-controller/internal/app/controller"
-	"github.com/RoboCup-SSL/ssl-vision-client/pkg/vision"
 	"github.com/gobuffalo/packr"
 	"log"
 	"net/http"
 )
 
 var address = flag.String("address", "localhost:8081", "The address on which the UI and API is served, default: localhost:8081")
-var visionAddress = flag.String("visionAddress", "224.5.23.2:10006", "The multicast address of ssl-vision, default: 224.5.23.2:10006")
 
 func main() {
 	flag.Parse()
 
 	setupGameController()
-	setupVisionClient()
 	setupUi()
 
 	err := http.ListenAndServe(*address, nil)
@@ -30,14 +27,6 @@ func setupGameController() {
 	gameController.Run()
 	// serve the bidirectional web socket
 	http.HandleFunc("/api/control", gameController.ApiServer.WsHandler)
-}
-
-func setupVisionClient() {
-	receiver := vision.NewReceiver()
-	publisher := vision.NewPublisher()
-	publisher.PackageProvider = receiver.ToPackage
-	http.HandleFunc("/api/vision", publisher.Handler)
-	go receiver.Receive(*visionAddress)
 }
 
 func setupUi() {
