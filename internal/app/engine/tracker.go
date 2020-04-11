a376ff64e215b8965393d3db7bb36fbfb6239446
Nicolai Ommer <nicolai.ommer@gmail.com>
2020-04-11 12:40:16 +0200

    Overwrite tracker state for GC with each new tracker frame

--- internal/app/engine/tracker.go
+++ internal/app/engine/tracker.go
@@ -1,7 +1,6 @@
 package engine
 
 import (
-	"github.com/RoboCup-SSL/ssl-game-controller/internal/app/geom"
 	"github.com/RoboCup-SSL/ssl-game-controller/internal/app/tracker"
 )
 
@@ -17,7 +16,8 @@ func (e *Engine) ProcessTrackerFrame(wrapperFrame *tracker.TrackerWrapperPacket)
 	}
 	e.gcState.TrackerState[*wrapperFrame.Uuid] = &state
 
-	// TODO update tracker GC state as well
+	// for now, all tracker sources update the GC state
+	e.gcState.TrackerStateGc = &state
 }
 
 func convertRobots(robots []*tracker.TrackedRobot) (rs []*Robot) {
@@ -39,43 +39,4 @@ func convertBalls(balls []*tracker.TrackedBall) *Ball {
 		Vel: balls[0].Vel,
 	}
 	return &ball
-}
-
-//func (e *Engine) steadyCommonlyAgreedOnBallPos() *geom.Vector2 {
-//	var ballPos *geom.Vector2
-//	for _, trackerState := range e.gcState.TrackerState {
-//		if trackerState.BallPos != nil &&
-//			trackerState.BallVel != nil {
-//			if trackerState.BallVel.Length() > ballSteadyThreshold {
-//				// ball is still moving
-//				return nil
-//			}
-//			if ballPos == nil {
-//				// first steady ball pos
-//				ballPos = trackerState.BallPos
-//			} else if ballPos.DistanceTo(trackerState.BallPos) > 0.1 {
-//				// additional steady ball pos that differs from the first one
-//				return nil
-//			}
-//		}
-//	}
-//	return ballPos
-//}
-
-//func (e *Engine) uniqueBallPoss() (ballPoss []*geom.Vector2) {
-//	for _, trackerState := range e.gcState.TrackerState {
-//		if trackerState.BallPos != nil && !containsPos(ballPoss, trackerState.BallPos) {
-//			ballPoss = append(ballPoss, trackerState.BallPos)
-//		}
-//	}
-//	return
-//}
-
-func containsPos(l []*geom.Vector2, v *geom.Vector2) bool {
-	for _, ballPos := range l {
-		if ballPos.DistanceTo(v) < 0.1 {
-			return true
-		}
-	}
-	return false
 }
