b90766dd17eb7c44dfa2eb40aea51a47f5993baa
Nicolai Ommer <nicolai.ommer@gmail.com>
2020-04-18 20:57:01 +0200

    Do not check for successful ball placement -> autoRefs

--- internal/app/engine/process_ballplacement.go
+++ internal/app/engine/process_ballplacement.go
@@ -28,29 +28,7 @@ func (c *BallPlacementCoordinator) process() {
 
 	remainingDistance := c.remainingPlacementDistance()
 
-	if e.ballSteady() && e.teamInFavorKeepsSufficientDistance() &&
-		float64(remainingDistance) <= e.gameConfig.BallPlacementTolerance {
-		// if opponent does not yet keep sufficient distance, that's not the placing teams fold,
-		// so just wait a bit more
-		if e.opponentTeamKeepsSufficientDistance() {
-			duration := float32(e.timeProvider().Sub(*c.ballPlacementStartTime).Seconds())
-			var distance *float32
-			if c.ballPlacementStartPos != nil && e.gcState.TrackerStateGc.Ball.Pos != nil {
-				distance = new(float32)
-				*distance = float32(c.ballPlacementStartPos.DistanceTo(e.gcState.TrackerStateGc.Ball.Pos.ToVector2()))
-			}
-			e.Enqueue(createGameEventChange(state.GameEvent_PLACEMENT_SUCCEEDED, state.GameEvent{
-				Event: &state.GameEvent_PlacementSucceeded_{
-					PlacementSucceeded: &state.GameEvent_PlacementSucceeded{
-						ByTeam:    e.currentState.Command.ForTeam,
-						TimeTaken: &duration,
-						Precision: &remainingDistance,
-						Distance:  distance,
-					},
-				},
-			}))
-		}
-	} else if goDur(e.currentState.CurrentActionTimeRemaining) <= 0 {
+	if goDur(e.currentState.CurrentActionTimeRemaining) <= 0 {
 		e.Enqueue(createGameEventChange(state.GameEvent_PLACEMENT_FAILED, state.GameEvent{
 			Event: &state.GameEvent_PlacementFailed_{
 				PlacementFailed: &state.GameEvent_PlacementFailed{
@@ -69,33 +47,4 @@ func (c *BallPlacementCoordinator) remainingPlacementDistance() float32 {
 	placementPos := c.gcEngine.currentState.PlacementPos
 	ballPos := c.gcEngine.gcState.TrackerStateGc.Ball.Pos.ToVector2()
 	return float32(placementPos.DistanceTo(ballPos))
-}
-
-func (e *Engine) teamInFavorKeepsSufficientDistance() bool {
-	radius := e.gameConfig.BallPlacementMinRobotDistance + robotRadius + distanceThreshold
-	if e.currentState.NextCommand != nil && e.currentState.NextCommand.ForTeam == nil {
-		radius = e.gameConfig.DistanceToBallInStop + robotRadius + distanceThreshold
-	}
-
-	var robots []*Robot
-	for _, robot := range e.gcState.TrackerStateGc.Robots {
-		if *robot.Id.Team == *e.currentState.Command.ForTeam {
-			robots = append(robots, robot)
-		}
-	}
-
-	return !e.robotsInsideRadius(robots, e.gcState.TrackerStateGc.Ball.Pos.ToVector2(), radius)
-}
-
-func (e *Engine) opponentTeamKeepsSufficientDistance() bool {
-	radius := e.gameConfig.DistanceToBallInStop + robotRadius + distanceThreshold
-
-	var robots []*Robot
-	for _, robot := range e.gcState.TrackerStateGc.Robots {
-		if *robot.Id.Team != *e.currentState.Command.ForTeam {
-			robots = append(robots, robot)
-		}
-	}
-
-	return !e.robotsInsideRadius(robots, e.gcState.TrackerStateGc.Ball.Pos.ToVector2(), radius)
 }
