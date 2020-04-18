2d5389251087faf7ac213171c6458070d50d81f6
Nicolai Ommer <nicolai.ommer@gmail.com>
2020-04-18 20:57:52 +0200

    Do halt from stop with continue

--- internal/app/statemachine/change_continue.go
+++ internal/app/statemachine/change_continue.go
@@ -24,11 +24,8 @@ func (s *StateMachine) processChangeContinue(newState *state.State) (changes []*
 	} else if newState.NextCommand != nil {
 		log.Printf("Continue with next command: %v", *newState.NextCommand)
 		changes = append(changes, s.newCommandChange(newState.NextCommand))
-	} else if *newState.Command.Type != state.Command_STOP {
+	} else if *newState.Command.Type != state.Command_HALT {
 		log.Println("Halting the game as there is no known next command to continue with")
-		// halt the game, if not in STOP.
-		// Rational: After ball placement and no next command, halt the game to indicate that manual action is required
-		// If in STOP, that was most likely triggered manually already and a suddenly halted game might be confusing and not intended
 		changes = append(changes, s.newCommandChange(state.NewCommandNeutral(state.Command_HALT)))
 	}
 	return
