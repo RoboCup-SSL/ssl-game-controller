d182c53ae09f5d6fe5fe629a077f4ed7696a6418
Nicolai Ommer <nicolai.ommer@gmail.com>
2021-02-19 20:39:34 +0100

    Publish referee messages to all interface addresses

--- internal/app/publish/publisher.go
+++ internal/app/publish/publisher.go
@@ -12,19 +12,19 @@ const maxDatagramSize = 8192
 // Publisher can publish state and commands to the teams
 type Publisher struct {
 	address string
-	conn    *net.UDPConn
+	conns   []*net.UDPConn
 }
 
 // NewPublisher creates a new publisher that publishes referee messages via UDP to the teams
 func NewPublisher(address string) (p *Publisher) {
 	p = new(Publisher)
 	p.address = address
-
+	p.conns = []*net.UDPConn{}
 	return
 }
 
 func (p *Publisher) connect() bool {
-	p.conn = nil
+	p.disconnect()
 
 	addr, err := net.ResolveUDPAddr("udp", p.address)
 	if err != nil {
@@ -32,38 +32,49 @@ func (p *Publisher) connect() bool {
 		return false
 	}
 
-	conn, err := net.DialUDP("udp", nil, addr)
-	if err != nil {
-		log.Printf("Could not connect to '%v': %v", addr, err)
-		return false
-	}
+	iaddrs, err := net.InterfaceAddrs()
+	for _, iaddr := range iaddrs {
+		ip := iaddr.(*net.IPNet).IP
+		if ip.To4() == nil {
+			continue
+		}
+		laddr := &net.UDPAddr{
+			IP: ip,
+		}
+		conn, err := net.DialUDP("udp", laddr, addr)
+		if err != nil {
+			log.Printf("Could not connect to '%v': %v", addr, err)
+			continue
+		}
 
-	if err := conn.SetWriteBuffer(maxDatagramSize); err != nil {
-		log.Printf("Could not set write buffer to %v.", maxDatagramSize)
-	}
-	log.Println("Publishing referee messages to", p.address)
+		if err := conn.SetWriteBuffer(maxDatagramSize); err != nil {
+			log.Printf("Could not set write buffer to %v.", maxDatagramSize)
+		}
+		log.Printf("Publishing referee messages to %s at %s", conn.RemoteAddr(), conn.LocalAddr())
 
-	p.conn = conn
+		p.conns = append(p.conns, conn)
+	}
 	return true
 }
 
 func (p *Publisher) disconnect() {
-	p.conn = nil
+	p.conns = []*net.UDPConn{}
 }
 
 func (p *Publisher) SendMessage(refereeMsg *state.Referee) {
-	if p.conn == nil && !p.connect() {
+	if len(p.conns) == 0 && !p.connect() {
 		return
 	}
-
-	bytes, err := proto.Marshal(refereeMsg)
-	if err != nil {
-		log.Printf("Could not marshal referee message: %v\nError: %v", refereeMsg, err)
-		return
-	}
-	_, err = p.conn.Write(bytes)
-	if err != nil {
-		log.Printf("Could not write message: %v", err)
-		p.disconnect()
+	for _, conn := range p.conns {
+		bytes, err := proto.Marshal(refereeMsg)
+		if err != nil {
+			log.Printf("Could not marshal referee message: %v\nError: %v", refereeMsg, err)
+			return
+		}
+		_, err = conn.Write(bytes)
+		if err != nil {
+			log.Printf("Could not write message: %v", err)
+			p.disconnect()
+		}
 	}
 }
