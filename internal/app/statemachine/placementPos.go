1716892ad63dc08c080536493f1e82df03a92a26
Nicolai Ommer <nicolai.ommer@gmail.com>
2022-07-27 20:35:07 +0200

    Update ball placement pos determiner
     - Cover all game events
     - Sort them by protobuf order
     - Crash if a game event is not handled

--- internal/app/statemachine/placementPos.go
+++ internal/app/statemachine/placementPos.go
@@ -4,6 +4,7 @@ import (
 	"github.com/RoboCup-SSL/ssl-game-controller/internal/app/config"
 	"github.com/RoboCup-SSL/ssl-game-controller/internal/app/geom"
 	"github.com/RoboCup-SSL/ssl-game-controller/internal/app/state"
+	"log"
 	"math"
 )
 
@@ -17,6 +18,7 @@ type BallPlacementPosDeterminer struct {
 // Location determines the ball placement position based on the game event type
 func (s *BallPlacementPosDeterminer) Location() *geom.Vector2 {
 	switch *s.Event.Type {
+	// Ball out of field events (stopping)
 	case state.GameEvent_BALL_LEFT_FIELD_TOUCH_LINE:
 		if s.Event.GetBallLeftFieldTouchLine().Location != nil {
 			location := s.Event.GetBallLeftFieldTouchLine().Location
@@ -28,63 +30,101 @@ func (s *BallPlacementPosDeterminer) Location() *geom.Vector2 {
 			*location.Y = float32(math.Copysign(y, float64(*location.Y)))
 			return s.validateLocation(location)
 		}
-		return s.validateLocation(nil)
+		return s.keepCurrentPlacementPos()
 	case state.GameEvent_BALL_LEFT_FIELD_GOAL_LINE:
 		if s.Event.GetBallLeftFieldGoalLine().Location != nil {
 			location := s.Event.GetBallLeftFieldGoalLine().Location
 			return s.ballPlacementLocationGoalLine(location)
 		}
-		return s.validateLocation(nil)
+		return s.keepCurrentPlacementPos()
+	case state.GameEvent_AIMLESS_KICK:
+		return s.validateLocation(s.Event.GetAimlessKick().KickLocation)
+
+	// Stopping Fouls
+	case state.GameEvent_ATTACKER_TOO_CLOSE_TO_DEFENSE_AREA:
+		return s.validateLocation(s.Event.GetAttackerTooCloseToDefenseArea().BallLocation)
+	case state.GameEvent_DEFENDER_IN_DEFENSE_AREA:
+		teamInFavor := s.Event.ByTeam().Opposite()
+		return s.penaltyKick(teamInFavor)
+	case state.GameEvent_BOUNDARY_CROSSING:
+		return s.validateLocation(s.Event.GetBoundaryCrossing().Location)
+	case state.GameEvent_KEEPER_HELD_BALL:
+		return s.validateLocation(s.Event.GetKeeperHeldBall().Location)
+	case state.GameEvent_BOT_DRIBBLED_BALL_TOO_FAR:
+		return s.validateLocation(s.Event.GetBotDribbledBallTooFar().Start)
+
+	case state.GameEvent_BOT_PUSHED_BOT:
+		return s.validateLocation(s.Event.GetBotPushedBot().Location)
+	case state.GameEvent_BOT_HELD_BALL_DELIBERATELY:
+		return s.validateLocation(s.Event.GetBotHeldBallDeliberately().Location)
+	case state.GameEvent_BOT_TIPPED_OVER:
+		return s.validateLocation(s.Event.GetBotTippedOver().BallLocation)
+
+	// Non-Stopping Fouls
+	case state.GameEvent_BOT_KICKED_BALL_TOO_FAST,
+		state.GameEvent_ATTACKER_TOUCHED_BALL_IN_DEFENSE_AREA,
+		state.GameEvent_BOT_CRASH_UNIQUE,
+		state.GameEvent_BOT_CRASH_DRAWN:
+		return s.keepCurrentPlacementPos()
+
+	// Fouls while ball out of play
+	case state.GameEvent_DEFENDER_TOO_CLOSE_TO_KICK_POINT,
+		state.GameEvent_BOT_TOO_FAST_IN_STOP,
+		state.GameEvent_BOT_INTERFERED_PLACEMENT:
+		return s.keepCurrentPlacementPos()
+
+	// Scoring goals
+	case state.GameEvent_POSSIBLE_GOAL:
+		return nil
+	case state.GameEvent_GOAL:
+		center := geom.NewVector2(0.0, 0.0)
+		return s.validateLocation(center)
+	case state.GameEvent_INVALID_GOAL:
+		return s.keepCurrentPlacementPos()
+
+	// Other events
+
+	case state.GameEvent_ATTACKER_DOUBLE_TOUCHED_BALL:
+		return s.validateLocation(s.Event.GetAttackerDoubleTouchedBall().Location)
+	case state.GameEvent_PLACEMENT_SUCCEEDED:
+		return s.keepCurrentPlacementPos()
 	case state.GameEvent_PENALTY_KICK_FAILED:
 		if s.Event.GetPenaltyKickFailed().Location != nil {
 			location := s.Event.GetPenaltyKickFailed().Location
 			return s.ballPlacementLocationGoalLine(location)
 		}
-		return s.validateLocation(nil)
-	case state.GameEvent_POSSIBLE_GOAL:
-		return nil
-	case state.GameEvent_AIMLESS_KICK:
-		return s.validateLocation(s.Event.GetAimlessKick().KickLocation)
-	case state.GameEvent_GOAL:
-		center := geom.NewVector2(0.0, 0.0)
-		return s.validateLocation(center)
-	case state.GameEvent_BOT_TIPPED_OVER:
-		return s.validateLocation(s.Event.GetBotTippedOver().BallLocation)
-	case state.GameEvent_BOT_INTERFERED_PLACEMENT:
-		return s.validateLocation(s.CurrentPlacementPos)
-	case state.GameEvent_BOT_DRIBBLED_BALL_TOO_FAR:
-		return s.validateLocation(s.Event.GetBotDribbledBallTooFar().Start)
-	case state.GameEvent_BOT_PUSHED_BOT:
-		return s.validateLocation(s.Event.GetBotPushedBot().Location)
-	case state.GameEvent_BOT_HELD_BALL_DELIBERATELY:
-		return s.validateLocation(s.Event.GetBotHeldBallDeliberately().Location)
-	case state.GameEvent_ATTACKER_DOUBLE_TOUCHED_BALL:
-		return s.validateLocation(s.Event.GetAttackerDoubleTouchedBall().Location)
-	case state.GameEvent_ATTACKER_TOO_CLOSE_TO_DEFENSE_AREA:
-		return s.validateLocation(s.Event.GetAttackerTooCloseToDefenseArea().BallLocation)
-	case state.GameEvent_BOUNDARY_CROSSING:
-		return s.validateLocation(s.Event.GetBoundaryCrossing().Location)
-	case state.GameEvent_DEFENDER_TOO_CLOSE_TO_KICK_POINT:
-		return s.validateLocation(s.CurrentPlacementPos)
-	case state.GameEvent_DEFENDER_IN_DEFENSE_AREA, state.GameEvent_MULTIPLE_CARDS:
-		teamInFavor := s.Event.ByTeam().Opposite()
-		location := geom.NewVector2(0, 0)
-		*location.X = float32((s.Geometry.FieldLength / 2.0) - s.Geometry.PenaltyKickDistToGoal)
-		if s.OnPositiveHalf[teamInFavor] {
-			*location.X *= -1
-		}
-		return location
-	case state.GameEvent_KEEPER_HELD_BALL:
-		return s.validateLocation(s.Event.GetKeeperHeldBall().Location)
+		return s.keepCurrentPlacementPos()
 	case state.GameEvent_NO_PROGRESS_IN_GAME:
 		return s.validateLocation(s.Event.GetNoProgressInGame().Location)
-	case state.GameEvent_PLACEMENT_FAILED:
-		return s.validateLocation(s.CurrentPlacementPos)
+	case state.GameEvent_PLACEMENT_FAILED,
+		state.GameEvent_MULTIPLE_CARDS,
+		state.GameEvent_MULTIPLE_FOULS,
+		state.GameEvent_BOT_SUBSTITUTION,
+		state.GameEvent_CHALLENGE_FLAG,
+		state.GameEvent_EMERGENCY_STOP,
+		state.GameEvent_UNSPORTING_BEHAVIOR_MINOR,
+		state.GameEvent_UNSPORTING_BEHAVIOR_MAJOR:
+		return s.keepCurrentPlacementPos()
 	case state.GameEvent_TOO_MANY_ROBOTS:
 		return s.validateLocation(s.Event.GetTooManyRobots().BallLocation)
+
 	default:
-		return s.validateLocation(nil)
+		log.Fatalln("Unhandled game event: ", *s.Event.Type)
+		return nil
 	}
+}
+
+func (s *BallPlacementPosDeterminer) keepCurrentPlacementPos() *geom.Vector2 {
+	return s.validateLocation(nil)
+}
+
+func (s *BallPlacementPosDeterminer) penaltyKick(teamInFavor state.Team) *geom.Vector2 {
+	location := geom.NewVector2(0, 0)
+	*location.X = float32((s.Geometry.FieldLength / 2.0) - s.Geometry.PenaltyKickDistToGoal)
+	if s.OnPositiveHalf[teamInFavor] {
+		*location.X *= -1
+	}
+	return location
 }
 
 // ballPlacementLocationGoalLine determines the placement location for the case that the ball left the field via goal line
