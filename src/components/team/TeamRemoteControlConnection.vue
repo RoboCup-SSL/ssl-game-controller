9e3649f53cc2e67532e6c50f14af2ce0340e7ebe
Nicolai Ommer <nicolai.ommer@gmail.com>
2022-11-12 15:49:32 +0100

    Revert unintentional change

--- src/components/team/TeamRemoteControlConnection.vue
+++ src/components/team/TeamRemoteControlConnection.vue
@@ -6,6 +6,13 @@
                     class="fa-xs"
                     icon="signal"/>
         </span>
+        <span v-help-text="'connection verified'"
+              class="connection-verified"
+              v-if="connectionVerified">
+            <font-awesome-icon
+                    class="fa-xs"
+                    icon="shield-alt"/>
+        </span>
         <span v-help-text="'Remote control not connected'"
               v-if="!connected">
             <font-awesome-icon
@@ -25,9 +32,15 @@
             connected() {
                 return this.$store.state.gcState.teamState[this.teamColor].remoteControlConnected
             },
+            connectionVerified() {
+                return this.$store.state.gcState.teamState[this.teamColor].remoteControlConnectionVerified
+            }
         }
     }
 </script>
 
 <style scoped>
+    .connection-verified {
+        margin-left: 0.3em;
+    }
 </style>
