89f1b2be531efb83059e5ba6fdf11cd7edb8aa04
Nicolai Ommer <nicolai.ommer@gmail.com>
2022-11-12 12:32:23 +0100

    Add a simple script that generates an architecture diagram

---
+++ doc/generate-architecture-diagram.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+readonly SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+readonly package_suffix="github.com/RoboCup-SSL/ssl-game-controller/internal/app"
+
+cd "${SCRIPT_DIR}/../internal/app"
+
+function generate_uml() {
+
+  echo "@startuml"
+
+  for f in **; do
+    if [[ -d "$f" ]]; then
+      echo "node \"$f\" {"
+      echo "}"
+    fi
+  done
+
+  echo ""
+
+  for f in **; do
+    if [[ -d "$f" ]]; then
+      grep -ohP "$package_suffix/\K.+(?=\")" $f/*.go | sort | uniq | sed "s~^\(.*\)~\"$f\" --> \"\\1\"~" || true
+    fi
+  done
+
+  echo "@enduml"
+}
+
+generate_uml > "${SCRIPT_DIR}/architecture.puml"
