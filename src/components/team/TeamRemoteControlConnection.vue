1dd5cb7561078f0451322f46843db381db1081e9
Nicolai Ommer <nicolai.ommer@gmail.com>
2022-11-12 15:27:18 +0100

    Convert communication txt files to puml files

--- src/components/team/TeamRemoteControlConnection.vue
+++ src/components/team/TeamRemoteControlConnection.vue
@@ -6,13 +6,6 @@
                     class="fa-xs"
                     icon="signal"/>
         </span>
-        <span v-help-text="'connection verified'"
-              class="connection-verified"
-              v-if="connectionVerified">
-            <font-awesome-icon
-                    class="fa-xs"
-                    icon="shield-alt"/>
-        </span>
         <span v-help-text="'Remote control not connected'"
               v-if="!connected">
             <font-awesome-icon
@@ -32,15 +25,9 @@
             connected() {
                 return this.$store.state.gcState.teamState[this.teamColor].remoteControlConnected
             },
-            connectionVerified() {
-                return this.$store.state.gcState.teamState[this.teamColor].remoteControlConnectionVerified
-            }
         }
     }
 </script>
 
 <style scoped>
-    .connection-verified {
-        margin-left: 0.3em;
-    }
 </style>
