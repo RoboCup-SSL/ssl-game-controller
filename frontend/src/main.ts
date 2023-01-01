89ab19157718a83781ec816eede3f6cc2f4fe6a1
Nicolai Ommer <nicolai.ommer@gmail.com>
2023-01-01 20:15:01 +0100

    Add Quasar as UI library

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,11 +4,19 @@ import './assets/main.css'
 
 import {createPinia} from "pinia";
 import router from './router'
+import { Quasar } from 'quasar'
 
 import {control} from "@/plugins/control";
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/dist/quasar.css'
 
 createApp(App)
   .use(router)
   .use(createPinia())
   .use(control)
+  .use(Quasar)
   .mount('#app')
