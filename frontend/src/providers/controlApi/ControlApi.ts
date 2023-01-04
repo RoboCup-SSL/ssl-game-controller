d4b4b5b508975936bc4665ba4aa76262a53f900a
Nicolai Ommer <nicolai.ommer@gmail.com>
2023-01-04 22:11:21 +0100

    Add control API methods

--- frontend/src/providers/controlApi/ControlApi.ts
+++ frontend/src/providers/controlApi/ControlApi.ts
@@ -1,4 +1,10 @@
 import {Input, Output} from "@/proto/ssl_gc_api";
+import {Change} from "@/proto/ssl_gc_change";
+import {Command} from "@/proto/ssl_gc_state";
+import type {GameEvent} from "@/proto/ssl_gc_game_event";
+import type {ContinueAction} from "@/proto/ssl_gc_engine";
+import type {Config} from "@/proto/ssl_gc_engine_config";
+import {Team} from "@/proto/ssl_gc_common";
 
 export class ControlApi {
   private readonly apiPath = '/api/control'
@@ -10,11 +16,55 @@ export class ControlApi {
     this.connect()
   }
 
+
+  public NewCommandNeutral(type: Command.Type) {
+    this.NewCommand(new Command({type, forTeam: Team.UNKNOWN}))
+  }
+
+  public NewCommandForTeam(type: Command.Type, forTeam: Team) {
+    this.NewCommand(new Command({type, forTeam}))
+  }
+
+  public NewCommand(command: Command) {
+    this.SubmitChange(Change.fromObject({
+      newCommandChange: {
+        command
+      }
+    }))
+  }
+
+  public AddGameEvent(gameEvent: GameEvent) {
+    gameEvent.origin = ["UI"]
+    this.SubmitChange(Change.fromObject({
+      addGameEventChange: {
+        gameEvent
+      }
+    }))
+  }
+
+  public ChangeConfig(configDelta: Config) {
+    this.Send(new Input({configDelta}))
+  }
+
+  public ResetMatch() {
+    this.Send(new Input({resetMatch: true}))
+  }
+
+  public Continue(continueAction: ContinueAction) {
+    this.Send(new Input({continueAction}))
+  }
+
+  public SubmitChange(change: Change) {
+    this.Send(new Input({change}))
+  }
+
   public Send(request: Input) {
     const ws = this.ws
     if (ws) {
-      const json = JSON.stringify(request)
+      const json = JSON.stringify(request.toObject())
       ws.send(json)
+    } else {
+      console.warn("No WebSocket connection. Dropping ", request.toObject())
     }
   }
 
