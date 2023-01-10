44d4fa684ccc6b1bc8b4fdec7fa990b89b7fcdb5
Nicolai Ommer <nicolai.ommer@gmail.com>
2023-01-10 19:38:47 +0100

    Add full dummy states to avoid some null-checks

--- frontend/src/store/gcState/index.ts
+++ frontend/src/store/gcState/index.ts
@@ -1,10 +1,30 @@
 import {defineStore} from "pinia";
-import {GcState} from "@/proto/ssl_gc_engine";
+import {GcState, TeamAdvantageChoice_AdvantageChoice} from "@/proto/ssl_gc_engine";
 
 export const useGcStateStore = defineStore('gcState', {
   state: () => {
     return {
-      gcState: GcState.fromJSON({})
+      gcState: {
+        teamState: {
+          YELLOW: {
+            connected: false,
+            connectionVerified: false,
+            remoteControlConnected: false,
+            remoteControlConnectionVerified: false,
+            advantageChoice: TeamAdvantageChoice_AdvantageChoice.UNRECOGNIZED,
+          },
+          BLUE: {
+            connected: false,
+            connectionVerified: false,
+            remoteControlConnected: false,
+            remoteControlConnectionVerified: false,
+            advantageChoice: TeamAdvantageChoice_AdvantageChoice.UNRECOGNIZED,
+          },
+        },
+        autoRefState: {},
+        trackers: {},
+        continueActions: [],
+      } as GcState
     }
   },
   getters: {},
--- frontend/src/store/matchState/index.ts
+++ frontend/src/store/matchState/index.ts
@@ -1,17 +1,73 @@
 import {defineStore} from "pinia";
-import {GameState_Type, State} from "@/proto/ssl_gc_state";
-import {Team} from "@/proto/ssl_gc_common";
+import {Command_Type, GameState_Type, State} from "@/proto/ssl_gc_state";
+import {Division, Team} from "@/proto/ssl_gc_common";
+import {MatchType, Referee_Stage} from "@/proto/ssl_gc_referee_message";
 
 export const useMatchStateStore = defineStore('matchState', {
   state: () => {
     return {
       matchState: {
+        stage: Referee_Stage.POST_GAME,
+        command: {
+          type: Command_Type.HALT,
+          forTeam: Team.UNKNOWN,
+        },
+        gameState: {
+          type: GameState_Type.HALT,
+          forTeam: Team.UNKNOWN,
+        },
+        stageTimeElapsed: {
+          seconds: 0,
+        },
+        stageTimeLeft: {
+          seconds: 0,
+        },
+        matchTimeStart: new Date(),
+        currentActionTimeRemaining: {
+          seconds: -5,
+        },
+        gameEvents: [],
+        proposalGroups: [],
+        division: Division.DIV_A,
+        firstKickoffTeam: Team.YELLOW,
+        matchType: MatchType.UNKNOWN_MATCH,
+        readyContinueTime: new Date(),
         teamState: {
           [Team.YELLOW]: {
-            goalkeeper: 5
+            name: "Unknown",
+            goals: 0,
+            goalkeeper: 5,
+            yellowCards: [],
+            redCards: [],
+            timeoutsLeft: 4,
+            timeoutTimeLeft: {
+              seconds: 61,
+            },
+            onPositiveHalf: true,
+            fouls: [],
+            ballPlacementFailures: 0,
+            ballPlacementFailuresReached: false,
+            canPlaceBall: false,
+            maxAllowedBots: 5,
+            challengeFlags: 3,
           },
           [Team.BLUE]: {
-            goalkeeper: 15
+            name: "Unknown",
+            goals: 2,
+            goalkeeper: 15,
+            yellowCards: [],
+            redCards: [],
+            timeoutsLeft: 4,
+            timeoutTimeLeft: {
+              seconds: 61,
+            },
+            onPositiveHalf: false,
+            fouls: [],
+            ballPlacementFailures: 0,
+            ballPlacementFailuresReached: false,
+            canPlaceBall: false,
+            maxAllowedBots: 5,
+            challengeFlags: 3,
           }
         }
       } as State
