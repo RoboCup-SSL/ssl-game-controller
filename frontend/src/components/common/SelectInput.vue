5da0724a47c5c9c7bbaec0b14678c66fbd553c4c
Nicolai Ommer <nicolai.ommer@gmail.com>
2023-01-10 21:30:31 +0100

    Add SelectInput

---
+++ frontend/src/components/common/SelectInput.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {computed, ref, watch} from "vue";
+
+const props = defineProps<{
+  model: string,
+  options: string[],
+}>()
+const emit = defineEmits(['onUpdate'])
+
+const model = ref(structuredClone(props.model))
+const value = computed(() => props.model)
+watch(value, (newValue) => {
+  model.value = newValue
+})
+
+const onBlur = () => {
+  if (model.value !== value.value) {
+    emit('onUpdate', model.value)
+    model.value = structuredClone(value.value)
+  }
+}
+</script>
+
+<template>
+  <q-select v-model="model" :options="options" />
+</template>
