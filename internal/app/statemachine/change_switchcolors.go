7d8bc4c5d02f7bc2b50def6a0871b382c6fa9985
Nicolai Ommer <nicolai.ommer@gmail.com>
2023-01-14 22:24:16 +0100

    Fix: Switch firstKickoffTeam when switching colors

--- internal/app/statemachine/change_switchcolors.go
+++ internal/app/statemachine/change_switchcolors.go
@@ -6,5 +6,8 @@ func (s *StateMachine) processChangeSwitchColors(newState *state.State) (changes
 	yellow := newState.TeamInfo(state.Team_YELLOW)
 	newState.TeamState[state.Team_YELLOW.String()] = newState.TeamInfo(state.Team_BLUE)
 	newState.TeamState[state.Team_BLUE.String()] = yellow
+
+	// 'FirstKickoffTeam' is bound to the team, not the color, so we have to also switch this
+	*newState.FirstKickoffTeam = newState.FirstKickoffTeam.Opposite()
 	return
 }
