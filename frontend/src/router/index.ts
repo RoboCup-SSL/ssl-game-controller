3a6f3f7b285ead0c9d8e9b1fd067d5107cc44a86
Nicolai Ommer <nicolai.ommer@gmail.com>
2023-05-01 13:23:37 +0200

    Change vue-router mode to webHashHistory
    This allows refreshing the page when the UI is served by the gc-binary.

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,9 +1,9 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import StartView from '../views/StartView.vue'
 import FullStateView from "@/views/FullStateView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
