b89e1e6b643043cad8e86cca0bb8ffe8adf22a71
Nicolai Ommer <nicolai.ommer@gmail.com>
2023-08-05 17:04:58 +0200

    Allow to disable halftime recommendations
    This is useful during development, when a match is automatically simulated indefinitely.

--- internal/app/engine/process_continue_next_action.go
+++ internal/app/engine/process_continue_next_action.go
@@ -102,7 +102,9 @@ func (e *Engine) nextActions() (actions []*ContinueAction, hints []*ContinueHint
 
 	if *e.currentState.Command.Type == state.Command_STOP ||
 		*e.currentState.Command.Type == state.Command_HALT {
-		if e.currentState.StageTimeLeft.AsDuration() < 0 && *e.currentState.Stage != state.Referee_PENALTY_SHOOTOUT {
+		if (e.gameConfig.RecommendHalfTimes || e.currentState.Stage.IsPausedStage()) &&
+			e.currentState.StageTimeLeft.AsDuration() < 0 &&
+			*e.currentState.Stage != state.Referee_PENALTY_SHOOTOUT {
 			actions = append(actions, createContinueAction(
 				ContinueAction_NEXT_STAGE,
 				state.Team_UNKNOWN,
